bfcc5d6161c685f101b122fe95a58a9208c19a3d
Ivars-1994 <Ivarsjaundzeikars531@gmail.com>
2025-09-18 10:29:44 +0200

    d

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -107,7 +107,7 @@ const Services = () => {
         <div className="max-w-4xl mx-auto mb-12 rounded-xl overflow-hidden shadow-md">
           <AspectRatio ratio={16/9}>
             <img 
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGr2rSc0DHgeXiFF9XJRzA1D01XvMdTdBPTg&s" 
+              src="https://www.selbststaendig.de/sites/default/files/artikelbilder/kammerjaeger.jpgd" 
               alt="Professionelle Schädlingsbekämpfung in Aktion durch zertifizierte Experten" 
               className="object-cover w-full h-full transition-transform duration-500 hover:scale-105"
               width="840"
